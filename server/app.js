b7a4b704eeced96653ad75fab734e79ed180058b
Alex <alex.sklv85@gmail.com>
2017-02-11 23:47:23 +0300

    add config for api

--- server/app.js
+++ server/app.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
+var config = require('./config.json');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 var api = require('./api/questions');
 app.use('/api/questions', api(app));
 
-var port = 3000;
-
+var port = config.port;
 app.listen(port, () => {
     console.log(`Bad Saita api starting on port ${port}!`);
 });
